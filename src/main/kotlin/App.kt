5c2157c2e8f4d581b5143b89ba407461505c4376
a.yazdi <ahmadyazdi243@gmail.com>
2024-02-20 13:15:40 +0330

    finish basics

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,4 @@
+import java.util.Date
 import kotlin.random.Random
 
 fun main(){
@@ -28,19 +29,24 @@ fun main(){
     mutableMessages[0] = "hi"
     mutableMessages.add("today?")
     println(mutableMessages)
+
         //read only list
     val readOnlyNumbers = listOf(1, 2, 3)
+
     //readOnlyNumbers[0] = 0  //can not modify
     println("second parameter of readOnlyNumbers: ${readOnlyNumbers[1]}")
     println("does readOnlyNumbers contains 3: ${3 in readOnlyNumbers}")
     println("count of elements in readOnlyNumbers: ${readOnlyNumbers.count()}")
 
     //Set
+
         //read-only
     val readOnlyFruits = setOf("apple", "banana", "cherry", "cherry") //cherry is duplicated
+
         //mutable
     val mutableFruits: MutableSet<String> = mutableSetOf("apple", "cherry", "banana")
     mutableFruits.add("apple")
+
     //mutableFruits[0] = "orange"  //As sets are unordered, you can't access an item at a particular index
     println("readOnlyFruits: $readOnlyFruits")
     println("mutableFruits: $mutableFruits")
@@ -48,6 +54,7 @@ fun main(){
     //Map
     val readOnlyFoodPrices: Map<String, Int> = mapOf("pizza" to 170, "sandwich" to 90)
     val mutableFoodPrices = mutableMapOf("pizza" to 170, "sandwich" to 90)
+
     //mutableFoodPrices.put("stake", 185)
     mutableFoodPrices["stake"] = 185
     println("readOnlyFoodPrices: $readOnlyFoodPrices")
@@ -57,6 +64,7 @@ fun main(){
 
 
     //conditional expressions
+
         //if
     val age = Random.nextInt(1, 36)
     if(age > 18){
@@ -99,6 +107,7 @@ fun main(){
         else -> "temp = $temp"
     }
     println("weather description: $description")
+
     // other range checks which can be use
     /*
         ..<
@@ -109,6 +118,7 @@ fun main(){
      */
 
     //loops
+
         //for
     for(i in 1..5){
         println("inside for: i = $i")
@@ -139,21 +149,86 @@ fun main(){
     printFactorial(5)
     val numbers = listOf(1, 5, 8, 6, 9, 7, 4, 531, 45, 58, 78, 54, 65)
     printOddNumbers(numbers)
+
         //named arguments
     printMessageWithPrefix(prefix = "log", message = "hello")
     printMessageWithPrefix("hello")
+
         //single expression function: look at the declaration
     println(sum(5, 4))
+
     //lambda expression
     println({msg: String -> msg.uppercase()}("hello"))
+
     //assign lambda expression to a variable
     val makeStringUppercase = {msg: String -> msg.uppercase()}
     println(makeStringUppercase("good bye"))
+
+    // define with function type
+    val makeStringLowerCase: (String) -> String = {str -> str.lowercase()}
+    println(makeStringLowerCase("WHATS UP"))
+
     //pass lambda expression to another function
     val evenNumbers = numbers.filter {x -> x % 2 == 0}
     val oddNumbers = numbers.filter {x -> x % 2 != 0}
+    val doubled = numbers.map {n -> n * 2}
+    val tripled = numbers.map {n -> n * 3}
     println("evenNumbers: $evenNumbers")
     println("oddNumbers: $oddNumbers")
+    println("doubledNumbers: $doubled")
+    println("tripledNumbers: $tripled")
+
+    //using functions with lambda return type
+    val timesInMinutes = listOf(2, 10, 15, 1)
+    val minutesToSeconds = toSeconds("minute")
+    val totalTimeInSeconds = timesInMinutes.map(minutesToSeconds).sum()
+    println("totalTimeInSeconds: $totalTimeInSeconds")
+
+    //classes
+    class Contact(var firstname: String, var lastname: String)
+    val c = Contact("ahmad", "yazdi")
+    c.firstname = "Ahmad"
+    c.lastname = "Yazdi"
+    println("contact: ${c.firstname} ${c.lastname}")
+
+    class Costumer{
+        var id: Long = 0
+        var email: String = ""
+        override fun toString(): String{
+            return "id: $id \nemail: $email"
+        }
+    }
+    val costumer = Costumer()
+    costumer.id = 1
+    costumer.email = "ahmad@gmail.com"
+    println("costumer: $costumer")
+
+        //data classes
+    data class Dog(var id: Long, var name: String)
+    val dog = Dog(1, "alex")
+    val dog2 = Dog(2, "teddi")
+    val dog3 = dog.copy()
+    println("dog: $dog") //don't need to override toString() function
+    println("dog equals dog2? ${dog == dog2}")
+    println(dog3)
+
+    //null safety
+    var neverNull: String = "This can't be null"
+
+        //neverNull = null  //this is not allowed
+    var nullable: String? = "this one can be null"
+    nullable = null
+
+        //printMessageWithPrefix(nullable)  //nullables can't be passed as function parameter
+    nullTaker(neverNull)
+    nullTaker(nullable)
+
+        //null safe calls
+    println(nullable?.length)
+
+        //Elvis operator ?:
+        //provide a default value to return if a null value is detected
+    println(nullable?.length ?:0)
 }
 
 
@@ -191,3 +266,16 @@ fun printMessageWithPrefix(message: String, prefix: String = "Info") {
 
 //single expression function
 fun sum(x: Int, y: Int) = x + y
+
+//Return lambda expression from a function
+fun toSeconds(time: String): (Int) -> Int = when(time){
+    "hour" -> {value -> value * 60 * 60}
+    "minute" -> {value -> value * 60}
+    else -> {value -> value}
+}
+
+//accept null in functions
+fun nullTaker(maybeNull: String?){
+    if(!maybeNull.isNullOrEmpty()) println("the value inside maybeNull is: $maybeNull")
+    else println("maybeNull is null or empty")
+}
